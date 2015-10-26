617cb5341aad3a50d14c2084757dcc14ac5ef455
LeaderOfTheNewSchool <allendayton@gmail.com>
2015-10-25 21:46:46 -0500

    server now serving webpages with correct content types.

--- server.go
+++ server.go
@@ -11,8 +11,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
+
+func main() {
+	var handle *requestHandler = &requestHandler{}
+	fmt.Println("Server started on port 8000.")
+
+	http.Handle("/login", handle)
+	http.ListenAndServe(":8000", new(requestHandler))
+}
 
 /**
  * SERVER IS PRETTY MUCH NON FUNCTIONAL AT THE MOMENT.
@@ -26,11 +36,34 @@ type requestHandler struct{}
  */
 func (handler *requestHandler) ServeHTTP(response http.ResponseWriter,
 	request *http.Request) {
-	response.Write([]byte(request.URL.Path))
+	var path string = string(request.URL.Path[1:])
+	//fmt.Println(path)
+	webpageData, error := ioutil.ReadFile(path)
+
+	if error == nil {
+		var contentType string = addContentType(path)
+
+		response.Header().Set("Content-Type", contentType)
+		response.Write(webpageData)
+	} else {
+		response.WriteHeader(404)
+		response.Write([]byte(http.StatusText(404)))
+	}
 	fmt.Println(request.URL.Path)
 }
-func main() {
-	var handle *requestHandler = &requestHandler{}
-	fmt.Println("Server started on port 8000.")
-	http.ListenAndServe(":8000", handle)
+
+func addContentType(path string) string {
+	var contentType string
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css; charset=utf-8"
+	} else if strings.HasSuffix(path, ".js") {
+		contentType = "application/js; charset=utf-8"
+	} else if strings.HasSuffix(path, ".html") {
+		contentType = "text/html; charset=utf-8"
+	} else if strings.HasSuffix(path, ".jpg") {
+		contentType = "image/jpg; charset=utf-8"
+	} else {
+		contentType = "text/plain; charset=utf-8"
+	}
+	return contentType
 }
